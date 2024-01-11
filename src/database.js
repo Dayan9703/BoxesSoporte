044d51dabd754e3118c3aaed36ab096b55679c7a
Dayan9703 <dayan.basultoreyes@gmail.com>
2024-01-11 11:40:32 -0500

    Conectando mongoAtlas

--- src/database.js
+++ src/database.js
@@ -2,10 +2,23 @@ const mongoose = require('mongoose')
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
 const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+const DB_URI = process.env.DB_URI;
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+},
+    (err, res) => {
+        if (!err) {
+            console.log('***Conected***')
+        } else {
+            console.log('***Error Conected***')
+        }
+    });
+
+/*mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 })
     .then(db => console.log('Database is connected'))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err));*/
